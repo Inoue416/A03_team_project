9296eae85e6b1539edb470d1ffc22c427a2d4dc0
Yuya Inoue <yuyatuta@icloud.com>
2023-02-20 15:53:48 +0900

    設定の修正

---
+++ a03_team_project/app/models/skills.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Skills(models.Model):
+    id = models.AutoField(primary_key=True)
+    skill = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'skills'
