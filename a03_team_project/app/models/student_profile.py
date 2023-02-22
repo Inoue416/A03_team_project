3f33c369a2c9c0be30150ed55591bd1afb6f0951
Yuya Inoue <yuyatuta@icloud.com>
2023-02-22 23:17:04 +0900

    student_profile.pyの編集

--- a03_team_project/app/models/student_profile.py
+++ a03_team_project/app/models/student_profile.py
@@ -1,14 +1,14 @@
 from django.db import models
-from app.models.students import Students
-from app.models.universities import Universities
-from app.models.skills import Skills
-
+from universities import Universities
+from grade import Grade
+from users import Users  # usersモデルの定義名によって変更
 
 class StudentProfile(models.Model):
-    student_id = models.OneToOneField(Students, on_delete=models.PROTECT)
+    # usersモデルの定義名によって変更
+    id = models.AutoField(primary_key=True)
+    student_id = models.OneToOneField(Users, on_delete=models.PROTECT)
     university_id = models.ForeignKey(Universities, on_delete=models.DO_NOTHING)
-    grade = models.CharField(max_length=5, null=False)
-    good_skill = models.ForeignKey(Skills, on_delete=models.DO_NOTHING, null=True)
+    grade = models.OneToOneField(Grade, on_delete=models.DO_NOTHING)
     icon = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
