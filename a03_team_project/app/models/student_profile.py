705abfd2c84f8db8754c4869290367672f99bebe
Yuya Inoue <yuyatuta@icloud.com>
2023-02-22 23:37:57 +0900

    制約をcascadeに変更

--- a03_team_project/app/models/student_profile.py
+++ a03_team_project/app/models/student_profile.py
@@ -6,9 +6,9 @@ from users import Users  # usersモデルの定義名によって変更
 class StudentProfile(models.Model):
     # usersモデルの定義名によって変更
     id = models.AutoField(primary_key=True)
-    student_id = models.OneToOneField(Users, on_delete=models.PROTECT)
-    university_id = models.ForeignKey(Universities, on_delete=models.DO_NOTHING)
-    grade = models.OneToOneField(Grade, on_delete=models.DO_NOTHING)
+    student_id = models.OneToOneField(Users, on_delete=models.CASCADE)
+    university_id = models.ForeignKey(Universities, on_delete=models.CASCADE)
+    grade = models.OneToOneField(Grade, on_delete=models.PROTECT)
     icon = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
