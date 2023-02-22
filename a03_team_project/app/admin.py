cd20d00fcc6fb3bf00f8999117ffdbd6b5db9448
Najah7 <najahmatsuo7@gmail.com>
2023-02-23 07:24:24 +0900

    ✨ ✅ User用のAdminを作成＆登録。（テストOK、未リファクタリング）

--- a03_team_project/app/admin.py
+++ a03_team_project/app/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from app.models.user import User
 
 # Register your models here.
+class UserAdmin(UserAdmin):
+    """AdminページのUserモデルの設定"""
+    ordering = ['id']
+    list_display = ['id', 'name', 'is_student', 'is_company', 'is_staff']
+    fieldsets = (
+        (_('Personal Info'), {'fields': ('name', 'email',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            # HACK:命名
+            _('dates Info'),
+            {
+                'fields' :(
+                    'last_login',
+                    'created_at',
+                    'updated_at',
+                )
+            }
+        )
+    )
+    readonly_fields = ['last_login', 'created_at', 'updated_at']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields' : (
+                'name',
+                'email',
+                'password1',
+                'password2',
+                'is_student',
+                'is_company',
+                'is_active',
+            )
+        }),
+    )
+    
+admin.site.register(User, UserAdmin)
