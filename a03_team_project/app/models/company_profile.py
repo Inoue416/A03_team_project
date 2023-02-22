7114e3f6a6fe1f773dd5a1eee99ee1ff2f87d44b
Yuya Inoue <yuyatuta@icloud.com>
2023-02-22 23:45:09 +0900

    company_profile.pyを編集

--- a03_team_project/app/models/company_profile.py
+++ a03_team_project/app/models/company_profile.py
@@ -1,11 +1,15 @@
 from django.db import models
-from app.models.companies import Companies
+from users import Users  # 他同様
 
 
 class CompanyProfile(models.Model):
-    company_id = models.OneToOneField(Companies, on_delete=models.PROTECT)
+    id = models.AutoField(primary_key=True)
+    company_id = models.OneToOneField(Users, on_delete=models.CASCADE)
     outline = models.TextField(null=False, default='作成中')
     businness_contents = models.TextField(null=False, default='作成中')
+    # 画像までのパスを格納予定
+    image = models.ImageField(null=True)
+    homepage = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
 
