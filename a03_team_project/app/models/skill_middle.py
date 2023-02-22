8f0e44b7bb98f97d93e1e7ed459cc96e2001eaa4
Yuya Inoue <yuyatuta@icloud.com>
2023-02-22 23:36:53 +0900

    制約をcascadeに変更

--- a03_team_project/app/models/skill_middle.py
+++ a03_team_project/app/models/skill_middle.py
@@ -4,8 +4,8 @@ from skills import Skills
 
 class SkillMiddle(models.Model):
     # idはUsersテーブルのキーをフォーリンキーとして用いる
-    id = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
-    skill_id = models.ForeignKey(Skills, on_delete=models.DO_NOTHING)
+    id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    skill_id = models.ForeignKey(Skills, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'skill_middle'
