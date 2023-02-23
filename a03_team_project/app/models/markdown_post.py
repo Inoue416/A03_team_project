e8a304869d3536df3fbc9fd2f795850c94b53ed9
Yuya Inoue <yuyatuta@icloud.com>
2023-02-24 00:26:04 +0900

    マークダウンのtitleフィールドを追加

--- a03_team_project/app/models/markdown_post.py
+++ a03_team_project/app/models/markdown_post.py
@@ -7,7 +7,8 @@ class MarkdownPost(models.Model):
     # TODO
     # 直接マークダウンを入れるかmdファイルをサーバーに保存し、それをクライアントに返す式にするかで
     # Char(URL)かText(markdown)か変わってくる
-    data = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=255, null=False)
+    data = models.TextField(null=False)
     # TODO
     # 画像の挿入も検討したい
     # image =     
