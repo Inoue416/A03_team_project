d465cf62357e5097af560088e6786b72cd261d19
Najah7 <najahmatsuo7@gmail.com>
2023-02-26 17:34:11 +0900

    ✅ ✨ Sinup用のフォーム作成（テスト通過）

--- a03_team_project/accounts/forms.py
+++ a03_team_project/accounts/forms.py
@@ -1,1 +1,23 @@
-        
+"""
+認証関係のフォーム
+"""
+from django.contrib.auth import get_user_model, password_validation
+from django.contrib.auth.forms import UserCreationForm
+
+from django import forms
+
+class SignupForm(UserCreationForm):
+    """サインアップ用のフォーム"""
+    
+    email = forms.EmailField(required=True, label='メールアドレス')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs.update({'class':'form-control', 'placeholder': "Email"})
+        self.fields["password1"].widget.attrs.update({'class': 'form-control', 'placeholder': "パスワード"})
+        self.fields["password2"].widget.attrs.update({'class': 'form-control', 'placeholder': "確認用のパスワード"})
+    
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ('email', 'password1', 'password2')
+            
