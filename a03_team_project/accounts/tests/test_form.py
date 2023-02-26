0043c3cf20b97f30862f6b484c4f9027b0c3a559
Najah7 <najahmatsuo7@gmail.com>
2023-02-26 17:30:58 +0900

    ✅ Signup用のフォームのテストを作成

--- a03_team_project/accounts/tests/test_form.py
+++ a03_team_project/accounts/tests/test_form.py
@@ -1,1 +1,51 @@
+"""
+Tests for auth forms. 
+"""
+from django.test import TestCase
+
+from accounts.forms import (
+    SignupForm,
+)
+defaults_data = {
+    'email': 'test@example.com',
+    'password': 'Tuser!23',
+}
+
+class TestSingupForm(TestCase):
+    """サインアップのフォームのテスト"""
     
+    def test_signup_with_vailid_value(self):
+        """通常の入力のサインアップのバリデーション"""
+        email= defaults_data['email']
+        password = defaults_data['password']
+        form = SignupForm(data={
+            'email': email,
+            'password1': password,
+            'password2': password,
+        })
+        
+        self.assertTrue(form.is_valid())
+        
+    def test_signup_with_invalid_email(self):
+        """Invalidなメールアドレスに対するバリデーションのテスト"""
+        email = 'invalid'
+        password = defaults_data['password']
+        form = SignupForm(data={
+            'email': email,
+            'password1': password,
+            'password2': password,
+        })
+        
+        self.assertFalse(form.is_valid())
+    
+    def test_signup_with_invalid_password(self):
+        """Invalidなパスワードに対するバリデーションのテスト"""
+        email = defaults_data['email']
+        password = 'password'
+        form = SignupForm(data={
+            'email': email,
+            'password1': password,
+            'password2': password,
+        })
+        
+        self.assertFalse(form.is_valid())
