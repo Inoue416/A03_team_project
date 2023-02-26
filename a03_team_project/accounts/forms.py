eb0be8d2cdc4d1610cc3f731f892bcb5f694eb51
Najah7 <najahmatsuo7@gmail.com>
2023-02-26 17:36:18 +0900

    ✅ ✨ Login用のフォームを実装（テスト通過）

--- a03_team_project/accounts/forms.py
+++ a03_team_project/accounts/forms.py
@@ -2,6 +2,7 @@
 認証関係のフォーム
 """
 from django.contrib.auth import get_user_model, password_validation
+from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.forms import UserCreationForm
 
 from django import forms
@@ -21,3 +22,23 @@ class SignupForm(UserCreationForm):
         model = get_user_model()
         fields = ('email', 'password1', 'password2')
             
+class LoginForm(forms.Form):
+    """ログイン用のフォーム"""
+    email = forms.EmailField(label='メールアドレス')
+    password = forms.CharField(
+        label='パスワード',
+        min_length=8,
+        widget=forms.PasswordInput(),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
+        
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        validate_password(password)
+        return password
+        
