184b777d4b1a0227dfe344593d223ba487b608a4
Najah7 <najahmatsuo7@gmail.com>
2023-02-28 18:24:57 +0900

    ✨ Formを確認したいので簡易的にViewを作成

--- a03_team_project/accounts/views.py
+++ a03_team_project/accounts/views.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
 
+
+from accounts.forms import SignupForm, LoginForm
+
+# NOTE:使えるかも
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
+class SignUpView(CreateView):
+    """サインアップのビュー"""
+    form_class = SignupForm
+    success_url = '/'
+    template_name = 'registration/signup.html'
+
+class LoginView(LoginView):
+    """ログインのビュー"""
+    authentication_form = LoginForm
+    template_name = 'registration/login.html'
+    redirect_authenticated_user = True
+    
+
+class LogoutView(LogoutView):
+    """ログアウトのビュー"""
+    # NOTE:ログインページでボタンをおしたら、ログアウトするようにするためにどうする？
+    template_name = 'registration/login.html'
+
+class PsswordChangeView():
+    """パスワード変更のビュー"""
