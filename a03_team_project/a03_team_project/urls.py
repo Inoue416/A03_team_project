f5c2a819111204777e2cad324670cf2a9c995d49
Yuya Inoue <yuyatuta@icloud.com>
2023-03-03 00:27:08 +0900

    index.htmlのurlを設定

--- a03_team_project/a03_team_project/urls.py
+++ a03_team_project/a03_team_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views.index_view import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view()),
 ]
