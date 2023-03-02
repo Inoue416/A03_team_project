7a65bff4d9615856f0ad96dbb48a39a88648d12f
Yuya Inoue <yuyatuta@icloud.com>
2023-03-03 00:29:12 +0900

    全てのテーブルを定義

--- a03_team_project/app/admin.py
+++ a03_team_project/app/admin.py
@@ -3,6 +3,17 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from app.models.user import User
+from app.models.student_profile import StudentProfile
+from app.models.company_profile import CompanyProfile
+from app.models.skills import Skills
+from app.models.skill_middle import SkillMiddle
+from app.models.universities import Universities
+from app.models.grade import Grade
+from app.models.department import Department
+from app.models.department_middle import DepartmentMiddle
+from app.models.follow import Follow
+from app.models.markdown_post import MarkdownPost
+from app.models.nice import Nice
 
 # Register your models here.
 class UserAdmin(UserAdmin):
@@ -47,5 +58,49 @@ class UserAdmin(UserAdmin):
             )
         }),
     )
-    
+
+class UniversitiesAdmin(admin.ModelAdmin):
+    pass
+
+class NiceAdmin(admin.ModelAdmin):
+    pass
+
+class SkillsAdmin(admin.ModelAdmin):
+    pass
+
+class SkillMiddleAdmin(admin.ModelAdmin):
+    pass
+
+class StudentProfileAdmin(admin.ModelAdmin):
+    pass
+
+class CompanyProfileAdmin(admin.ModelAdmin):
+    pass
+
+class GradeAdmin(admin.ModelAdmin):
+    pass
+
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+class DepartmentMiddleAdmin(admin.ModelAdmin):
+    pass
+
+class FollowAdmin(admin.ModelAdmin):
+    pass
+
+class MarkdownPostAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Universities, UniversitiesAdmin)
+admin.site.register(Nice, NiceAdmin)
+admin.site.register(Skills, SkillsAdmin)
+admin.site.register(SkillMiddle, SkillMiddleAdmin)
+admin.site.register(StudentProfile, StudentProfileAdmin)
+admin.site.register(CompanyProfile, CompanyProfileAdmin)
+admin.site.register(Grade, GradeAdmin) 
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(DepartmentMiddle, DepartmentMiddleAdmin)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(MarkdownPost, )
