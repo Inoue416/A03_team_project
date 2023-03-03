356ccdbe6172ff031cbb4a06f1accf368c013bd1
Najah7 <najahmatsuo7@gmail.com>
2023-03-04 06:15:19 +0900

    ✨ Signup ViewとLogin Viewにメッセージ表示機能を追加。

--- a03_team_project/accounts/views.py
+++ a03_team_project/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
-
+from django.contrib import messages
 
 from accounts.forms import SignupForm, LoginForm
 
@@ -16,6 +16,20 @@ class SignUpView(CreateView):
     form_class = SignupForm
     success_url = '/'
     template_name = 'registration/signup.html'
+    
+    # NOTE:messageの表示機能を追加したいがためのオーバライド
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+        """
+        form = self.get_form()
+        if form.is_valid():
+            messages.info(request, 'サインアップしました')
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
 
 class LoginView(LoginView):
     """ログインのビュー"""
@@ -23,10 +37,17 @@ class LoginView(LoginView):
     template_name = 'registration/login.html'
     redirect_authenticated_user = True
     
-# NOTE:template_nameだけなので、urls.pyで指定していいかも
-# class LogoutView(LogoutView):
-#     """ログアウトのビュー"""
-#     template_name = 'registration/login.html'
-
-class PsswordChangeView():
-    """パスワード変更のビュー"""
+    # NOTE:messageの表示機能を追加したいがためのオーバライド
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+        """
+        
+        form = self.get_form()
+        if form.is_valid():
+            messages.info(request, 'ログインしました。')
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
