43ac1a93dd877e456da291bea4e1d6871b0b65f0
Najah7 <najahmatsuo7@gmail.com>
2023-03-04 06:16:22 +0900

    🚚 Django組み込みのパスワード変更Viewへのルーティングを追加

--- a03_team_project/accounts/urls.py
+++ a03_team_project/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 from accounts.views import SignUpView, LoginView
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
