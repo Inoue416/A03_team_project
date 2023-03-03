196ed8c27fbfac9b5ad68f0af39b03c17517761d
Najah7 <najahmatsuo7@gmail.com>
2023-03-03 14:30:41 +0900

    🚚 logoutのページのルーティング

--- a03_team_project/accounts/urls.py
+++ a03_team_project/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+
 from accounts.views import SignUpView, LoginView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
 ]
