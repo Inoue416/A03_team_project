bc26dabbaa10b99ec636209a5c2ea22d51c25da2
Najah7 <najahmatsuo7@gmail.com>
2023-03-04 09:21:15 +0900

    🚚 カスタムViewを適用するためのパスの修正

--- a03_team_project/accounts/urls.py
+++ a03_team_project/accounts/urls.py
@@ -1,12 +1,10 @@
-from django.urls import path, include
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.urls import path
 
-from accounts.views import SignUpView, LoginView
+from accounts.views import SignUpView, LoginView, CustomLogoutView, CustomPasswordChangeView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
-    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
-    path('password_change_done/', PasswordChangeDoneView.as_view(), name='password_change_done')
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
 ]
