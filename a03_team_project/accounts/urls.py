0a760ebb04895d17a43944073b5baaa91bbb071d
Najah7 <najahmatsuo7@gmail.com>
2023-03-04 09:49:24 +0900

    🚚 ログインしていな状態で、パスワード変更やログインなどを押した場合にログインぺ時に行くようにした
    正規表現をしっかり思い出せなったので、とりあえずの応急処置の状態です。
    正規表現について、正規表現についてちゃんと見直したら、しっかりした正規表現に書き直します🙇

--- a03_team_project/accounts/urls.py
+++ a03_team_project/accounts/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from accounts.views import SignUpView, LoginView, CustomLogoutView, CustomPasswordChangeView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
+    
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    # HACK:しっかりした正規表現に書き換えて
+    re_path(r'[login/]+', LoginView.as_view(), name='login'),
+    # path('login/', LoginView.as_view(), name='login'),
 ]
