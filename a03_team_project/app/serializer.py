e2b0cfbd61171e45a31e46bc8f54b11267d2bf6a
Yuya Inoue <yuyatuta@icloud.com>
2023-03-06 10:53:14 +0900

    シリアライザー削除

--- a03_team_project/app/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from app.models.nice import Nice
-
-class NiceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Nice
-        fields = ('markdown', 'user')
