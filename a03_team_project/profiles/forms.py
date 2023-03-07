3cddfb47a7e672dc1d59b60542b259321a04eeb9
木村謙介 <kimurakensuke@kimurakensukenoMacBook-Air.local>
2023-03-07 21:52:14 +0900

    companyのプロフィール登録(skill以外)実装

--- a03_team_project/profiles/forms.py
+++ a03_team_project/profiles/forms.py
@@ -1,6 +1,9 @@
+from dataclasses import fields
+from this import s
 from django import forms
+from betterforms.multiform import MultiModelForm
 
-from app.models import CompanyProfile
+from app.models import CompanyProfile, StudentProfile, Universities, Grade
 
 class CompanyForm(forms.ModelForm):
 
@@ -19,18 +22,75 @@ class CompanyForm(forms.ModelForm):
             self.login_user = user
             super().__init__(*args, **kwargs)
 
-        def save(self, request, **kwargs):
-
-            company_id = request.user.id
+        def save(self, **kwargs):
 
             company = CompanyProfile(
-                company_id = company_id,
-                outline = self.cleaned_data['outline'],
-                businness_contents = self.cleaned_data['businness_contents'],
-                image = self.cleaned_data['image'],
-                homepage = self.cleaned_data['homepage'],
-            )
+                    outline = self.cleaned_data['outline'],
+                    businness_contents = self.cleaned_data['businness_contents'],
+                    image = self.cleaned_data['image'],
+                    homepage = self.cleaned_data['homepage'],
+                )
 
-            company.save()
+            company.save()  
+
 
             return company
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentProfile
+        fields = ['icon']
+
+        labels = {
+            'icon': 'プロフィール画像'
+        }
+
+        # def save(self, commit=True):
+        #     student = super(StudentForm, self).save(commit=commit)
+
+        #     if commit:
+        #         student.save()
+
+        #     return student
+
+class UniversityForm(forms.ModelForm):
+    class Meta:
+        model = Universities
+        fields = ['name']
+
+        labels = {
+            'name': '大学'
+        }
+
+class GradeForm(forms.ModelForm):
+    class Meta:
+        model = Grade
+        fields = ['grade']
+
+        labels = {
+            'grade': '学年'
+        }
+
+class StudentMultiForm(MultiModelForm):
+
+    class Meta:
+        form_classes = {
+            'StudentProfile': StudentForm,
+            'Universities': UniversityForm,
+            'Grade': GradeForm,
+        }
+
+        def __init__(self, user, *args, **kwargs):
+            self.login_user = user
+            super().__init__(*args, **kwargs)
+
+        def save(self, **kwargs):
+            student = StudentProfile(
+                university = self.cleaned_data['name'],
+                grade = self.cleaned_data['grade'],
+                icon = self.cleaned_data['icon']
+            ),
+
+            student.save()
+
+            return student
