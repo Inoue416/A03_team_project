65c9ed4a3a993a4e9b09d838dba778053dffb932
Yuya Inoue <yuyatuta@icloud.com>
2023-03-07 19:24:45 +0900

    index_viewを作成

--- a03_team_project/app/views/index_view.py
+++ a03_team_project/app/views/index_view.py
@@ -1,11 +1,25 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from app.models.markdown_post import MarkdownPost
+from app.models.nice import Nice
+from app.models.comment import Comment
 
 
-
-class IndexView(ListView):
+# TODO: いいねとコメント数の集計
+class IndexView(TemplateView):
     template_name = "index.html"
-    model = MarkdownPost
-    ordering = ["-created_at"]
     context_object_name  = "markdown_objects"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        markdown_objects = []
+        markdown_data = MarkdownPost.objects.order_by('-created_at').all()
+        for md_data in markdown_data:
+            nice_num = Nice.objects.filter(markdown=md_data).count()
+            comment_num = Comment.objects.filter(markdown=md_data).count()
+            markdown_objects.append({
+                'md_data': md_data,
+                'nice_num': nice_num, 
+                'comment_num': comment_num
+            })
+        context['markdown_objects'] = markdown_objects
+        return context
     
