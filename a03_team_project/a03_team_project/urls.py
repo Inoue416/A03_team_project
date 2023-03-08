d59bdb5c75a2bfe97fa0a86a2a7377362422f110
Yuya Inoue <yuyatuta@icloud.com>
2023-03-08 15:26:40 +0900

    修正を行った

--- a03_team_project/a03_team_project/urls.py
+++ a03_team_project/a03_team_project/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.urls import path
 from app.views.index_view import IndexView
 from app.views.markdown_detail_view import MarkdownDetailView
@@ -25,10 +24,10 @@ from app.views.nice_view import NiceView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    # path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', IndexView.as_view(), name='home'),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
     path('markdown_detail/<int:id>', MarkdownDetailView.as_view(), name="markdown_detail"),
     path('comment/<int:markdown_id>', CommentView.as_view(), name="comment"),
     path('api/nice', NiceView.as_view(), name="nice")
--- a03_team_project/app/views/index_view.py
+++ a03_team_project/app/views/index_view.py
@@ -1,11 +1,12 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from app.models.markdown_post import MarkdownPost
 from app.models.nice import Nice
 from app.models.comment import Comment
 
 
 # TODO: いいねとコメント数の集計
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "index.html"
     context_object_name  = "markdown_objects"
     def get_context_data(self, **kwargs):
